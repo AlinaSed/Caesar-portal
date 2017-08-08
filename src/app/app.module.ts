b9f3eb50953657d92cba838df0cfbe6ddf8641e9
AlinaSed <alina.sedletska@gmail.com>
2017-08-08 11:59:51 +0300

    Caesar-app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent }          from './app.component';
+import { CaesarPortalComponent } from './caesar-portal/caesar-portal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaesarPortalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
